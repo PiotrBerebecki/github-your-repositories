985c4f821656d7c80a9650794118c0a530bac386
PiotrBerebecki <petebere@gmail.com>
2017-02-19 21:37:26 +0000

    Add header

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="repos">
+
+        <div className="repos__header">
+          <h3>Your repositories 20</h3>
+          <span className="repos__count">
+            20
+          </span>
+        </div>
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
