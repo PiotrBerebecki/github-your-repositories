cf23ad1135a5209838efc898f17fe4641aa865a1
PiotrBerebecki <petebere@gmail.com>
2017-02-20 07:31:41 +0000

    Style filters

--- src/App/index.js
+++ src/App/index.js
@@ -4,19 +4,45 @@ import './index.css';
 class App extends Component {
   constructor() {
     super();
+    this.changeFilter = this.changeFilter.bind(this);
+
     this.state = {
-      count: 88
+      count: 88,
+      filterActive: 'All'
     };
   }
 
+  changeFilter(e) {
+    this.setState({
+      filterActive: e.target.id
+    });
+  }
+
   render() {
+    const { count, filterActive } = this.state;
+
+    const renderFilterItems = ['All', 'Public', 'Private', 'Sources', 'Forks']
+      .map((item, index) => {
+        return (
+          <li className="filter__item" key={`${item}`}>
+            <button
+              className={item === filterActive ? 'filter__btn filter__btn--active filter__btn--' + item : `filter__btn`}
+              id={item}
+              onClick={this.changeFilter}
+            >
+              {item}
+            </button>
+          </li>
+        );
+      });
+
     return (
-      <div className="repos">
+      <div className="widget-repos">
 
         <section className="header">
           <h3 className="header__title">Your repositories</h3>
           <span className="header__count">
-            {this.state.count}
+            {count}
           </span>
         </section>
 
@@ -28,17 +54,16 @@ class App extends Component {
           />
         </section>
 
-        <section className="tabs">
-          <ul className="tabs__list">
-            <li className="tabs_item">All</li>
-            <li className="tabs_item">Public</li>
-            <li className="tabs_item">Private</li>
-            <li className="tabs_item">Sources</li>
-            <li className="tabs_item">Forks</li>
+        <section className="filter">
+          <ul className="filter__list">
+            {renderFilterItems}
           </ul>
         </section>
 
-
+        <section className="repos">
+          CSS
+          js
+        </section>
 
       </div>
     );
